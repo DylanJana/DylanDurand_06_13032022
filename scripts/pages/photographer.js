b2f95a5bcb9615234dfd010d13584cb160d537f4
DylanJana <dylan.durand88@outlook.fr>
2022-03-13 16:44:26 +0100

    Mise en place du filtre du photographe selon son ID

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,13 +1,12 @@
 /* Je récupére l'ID du photograph */
 const id = window.location.search.split('id=')[1];
-console.log(id)
 class App {
     constructor() {
         this.photographHeader = document.querySelector('.photograph-header')
         this.photographersApi = new PhotographersApi('/data/photographers.json')
     }
 
-    async main() {
+    async photograph() {
         const photographers = await this.photographersApi.getPhotographers()
         photographers
             .map(photographer => new Photograph(photographer) )
@@ -21,4 +20,4 @@ class App {
 }
 
 const app = new App()
-app.main()
+app.photograph()
