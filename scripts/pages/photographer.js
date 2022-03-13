ee7c8bf2ed5ef505f73d19559494090df3098509
DylanJana <dylan.durand88@outlook.fr>
2022-03-13 16:33:37 +0100

    Mise en place de la génération dynamique du contenu

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,23 @@
-//Mettre le code JavaScript lié à la page photographer.html
+/* Je récupére l'ID du photograph */
+const id = window.location.search.split('id=')[1];
+console.log(id)
+class App {
+    constructor() {
+        this.photographHeader = document.querySelector('.photograph-header')
+        this.photographersApi = new PhotographersApi('/data/photographers.json')
+    }
+
+    async main() {
+        const photographers = await this.photographersApi.getPhotographers()
+        photographers
+            .map(photographer => new Photograph(photographer) )
+            .forEach(photographer => {
+                console.log(photographer)
+                const Template = new  PhotographTemplate(photographer)
+                this.photographHeader.appendChild(Template.createTemplatePhotograph())
+            });
+    }
+}
+
+const app = new App()
+app.main()
--- scripts/templates/profil.js
+++ scripts/templates/profil.js
@@ -5,12 +5,11 @@ class ProfilPhotographer {
 
     createProfilPhotographer() {
         const $article = document.createElement('article');
-        //$article.classList.add('')
 
         const profilPhotographer = `
-            <a href="#" role="link">
+            <a href="photographer.html?id=${this._photographer.id}" role="link">
                 <img src="${this._photographer.portrait}" alt="${this._photographer.name}" role="image" class="border-radius--100">
-                <h2 role="heading" class="mbt-md title--xl">${this._photographer.name}</h2>
+                <h2 role="heading" class="mbt-md title--xl color-secondary">${this._photographer.name}</h2>
             </a>
             <h3 role="heading" class="margin--0 color-primary">${this._photographer.location}</h3>
             <p class="mbt-sm title--sm">${this._photographer.tagline}</p>
