940d8d47237db7ef2477597601fc7e2da71400b1
Dylan Durand <dylan.durand88@outlook.fr>
2022-05-21 19:10:50 +0200

    Dernières modifications

--- scripts/pages/app.js
+++ scripts/pages/app.js
@@ -14,14 +14,6 @@ fetchPhotographersJSON()
         }
     })
     .finally(()=>{
-        Array.from(spanTags, tag =>{
-            tag.addEventListener("click", () => {filterByTag(tag)})
-            tag.addEventListener("keypress", (e) => {
-                if(e.key === "Enter"){
-                    filterByTag(tag)
-                }
-            })
-        })
         window.addEventListener("scroll", () => {
             let anchor = document.querySelector(".anchor");
             let y = window.scrollY;
@@ -45,46 +37,6 @@ const addPhotographerInDOM = (photographer) =>{
                                 <p class="photographer__text" tabindex="0"><span class="photographer__localisation">${photographer.city}, ${photographer.country}</span>
                                 <span class="photographer__tagline">${photographer.tagline}</span>
                                 <span class="photographer__price">${photographer.price}€/jour</span></p> `;
-}
-
-const filterByTag = (tag) =>{
-    flushPhotographersInDOM();
-    if(tag.classList.contains("tags-active")){
-        tag.classList.toggle("tags-active");
-        tag.setAttribute("aria-label", `tag ${tag.textContent.slice(1)} désélectionné`)
-        tag.setAttribute("aria-label", `tag ${tag.textContent.slice(1)}`)
-        tag.removeAttribute("aria-selected");
-        tag.blur();
-        photographers.forEach(photographer =>{
-            addPhotographerInDOM(photographer);
-        });
-    } else{
-        for(let i=spanTags.length; i > 0; i--){
-            if(spanTags[i-1].classList.contains("tags-active")){
-                spanTags[i-1].classList.toggle("tags-active");
-                spanTags[i-1].setAttribute("aria-label", `tag ${spanTags[i-1].textContent.slice(1)}`);
-                spanTags[i-1].removeAttribute("aria-selected");
-                break;
-            }
-        }
-        tag.classList.toggle("tags-active");
-        tag.setAttribute("aria-label", `tag ${tag.textContent.slice(1)} sélectionné`)
-        tag.setAttribute("aria-selected", "true")
-        for(let i=0; i < photographers.length; i++){
-            for(let j=photographers[i].tags.length; j > 0; j--){
-                if("#"+ photographers[i].tags[j-1].charAt(0).toUpperCase() + photographers[i].tags[j-1].slice(1) == tag.textContent ){
-                    addPhotographerInDOM(photographers[i]);
-                    break;
-                }
-            }
-        }
-    }
-}
-
-const flushPhotographersInDOM = () =>{
-    while(wrapperPhotographers.firstChild){
-        wrapperPhotographers.firstChild.remove();
-    }
 }
 
 
