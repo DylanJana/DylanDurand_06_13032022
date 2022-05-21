f152d483585729038a29f35e9f37799b4aa4c5f8
Dylan Durand <dylan.durand88@outlook.fr>
2022-05-21 21:01:52 +0200

    Dernière modification CSS / Js

--- scripts/pages/features/lightbox.js
+++ scripts/pages/features/lightbox.js
@@ -5,15 +5,16 @@
  export class Lightbox {
     static init() {
       const photos = Array.from(document.querySelectorAll(".media__photo"));
+      const titlePicture = photos.map(photo => photo.getAttribute("alt"));
       const gallery = photos.map(photo => photo.getAttribute("src"));
       photos.forEach(photo => photo.addEventListener("click", e => {
         e.preventDefault();
-        new Lightbox(e.currentTarget.getAttribute("src"), gallery);
+        new Lightbox(e.currentTarget.getAttribute("src"), e.currentTarget.getAttribute("alt"), titlePicture, gallery);
       }));
       photos.forEach(photo => photo.addEventListener("keypress", e => {
         if(e.key === "Enter"){
           e.preventDefault();
-          new Lightbox(e.currentTarget.getAttribute("src"), gallery);
+          new Lightbox(e.currentTarget.getAttribute("src"),  e.currentTarget.getAttribute("alt"), titlePicture, gallery);
         }
       }));
     }
@@ -22,12 +23,14 @@
        * @param {string} src Source du media
        * @param {string[]} gallery Chemin des medias de la lightbox
        */
-    constructor(src, gallery) {
+    constructor(src, alt, title, gallery) {
       this.count = 0;
       this.element = this.buildLightboxInDOM();
-      this.addMediaInLightbox(src);
+      this.addMediaInLightbox(src, alt);
       this.onKeyUp = this.onKeyUp.bind(this);
       this.src = src;
+      this.alt = alt;
+      this.title = title;
       this.gallery = gallery;
       document.body.appendChild(this.element);
       document.addEventListener("keyup", this.onKeyUp);
@@ -44,7 +47,7 @@
       dom.setAttribute("role", "dialog");
       dom.innerHTML = `<button class="lightbox__close">Fermer</button>
                       <button class="lightbox__next">Suivant</button>
-                      <div class="lightbox__container"></div>
+                      <div class="lightbox__container"><div></div></div>
                       <button class="lightbox__prev">Précédent</button>`;
       dom.querySelector(".lightbox__close").addEventListener("click", this.closeLightbox.bind(this));
       dom.querySelector(".lightbox__next").addEventListener("click", this.nextMedia.bind(this));
@@ -56,18 +59,22 @@
        * Ajoute le media dans la lightbox
        * @param {string} src Source du media
        */
-    addMediaInLightbox(src){
-      const container = this.element.querySelector(".lightbox__container");
+    addMediaInLightbox(src, alt){
+      const container = this.element.querySelector(".lightbox__container div");
       container.innerHTML = "";
       this.src = src;
+      this.alt = alt;
+      const titleImage = document.createElement('h2');
+      titleImage.innerHTML = `${alt}`;
       if(src.includes(".jpg") || src.includes(".jpeg") || src.includes(".png")){
         const image = new Image();
         container.appendChild(image);
         image.classList.add("lightbox__photo");
         image.src = src;
       } else {
-        container.innerHTML = `<video width="100" heigth="100" controls src="${src}" type="video/mp4" class="lightbox__video">Sorry, your browser doesn't support embedded videos.</video>`
+        container.innerHTML = `<video width="100" heigth="100" controls src="${src}" alt="${alt}" type="video/mp4" class="lightbox__video">Sorry, your browser doesn't support embedded videos.</video>`;
       }
+      container.appendChild(titleImage);
     }
     /**
        *
@@ -101,12 +108,14 @@
     nextMedia(e) {
       e.preventDefault();
       let pos = this.gallery.findIndex(media => media === this.src);
+      console.log("pos ", pos);
+      console.log("this.title ", this.title[pos])
       if(pos >= this.gallery.length-1){
           pos = 0;
       } else{
           pos++;
       }
-      this.addMediaInLightbox(this.gallery[pos]);
+      this.addMediaInLightbox(this.gallery[pos], this.title[pos]);
     }
       /**
        * Aller au media précédent
@@ -120,7 +129,7 @@
       } else{
           pos--;
       }
-      this.addMediaInLightbox(this.gallery[pos]);
+      this.addMediaInLightbox(this.gallery[pos], this.title[pos]);
   
     }
   }
