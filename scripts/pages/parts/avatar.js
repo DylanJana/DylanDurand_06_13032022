851aa1c2a2b651dca9319ce3fe7db88c7fc0edf0
Dylan Durand <dylan.durand88@outlook.fr>
2022-05-21 19:13:25 +0200

    Dernières modifications

--- scripts/pages/parts/avatar.js
+++ scripts/pages/parts/avatar.js
@@ -1,6 +1,5 @@
 export const addAvatarToPhotographer = (name) =>{
     switch (name){
-        //case "Mimi Keel": return "Mimi/Portrait_Nora.jpg";
         case "Mimi Keel": return "Photographers_ID_Photos/MimiKeel.jpg";
         case "Ellie-Rose Wilkens": return "Photographers_ID_Photos/EllieRoseWilkens.jpg";
         case "Tracy Galindo": return "Photographers_ID_Photos/TracyGalindo.jpg";
